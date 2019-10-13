3817964f7338c20e64f469a6e0e4666ceed6f46d
Ildar <dilmuha@mail.ru>
2019-10-13 15:17:47 +0300

    Start

---
+++ MakeOrBuy/MakeOrBuy/Criterion.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeOrBuy
+{
+    public class Criterion
+    {
+        static List<Criterion>criterions;
+        private string name, description;
+        public Criterion(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+        }
+
+        public static List<Criterion> getCriterions
+        {
+            get
+            {
+                return criterions;
+            }
+        }
+
+        private static void generateCriterionsFirst()
+        {
+            criterions = new List<Criterion>();
+            criterions.Add(
+                new Criterion("Маржинальная рентабельность компонента",
+                "Принимает значение от 5% до 150%"));
+            criterions.Add(
+                new Criterion("Доля чистой прибыли в маржинальной рентабельности компонента",
+                "Принимает значение от 0% до 100%"));
+            criterions.Add(
+               new Criterion("Доля полезных затрат в постоянных издержках компонента",
+               "Принимает значение от 10% до 100%"));
+            criterions.Add(
+               new Criterion("Поведение затрат на покупку компонента у поставщиков",
+               "Баллы: 0 - дегрессивные, 1 - пропорциональные, 2 - прогрессивные."));
+
+        }
+
+        private static void generateCriterionsSecond()
+        {
+            criterions = new List<Criterion>();
+            criterions.Add(
+                new Criterion("Перспективы производства компонента",
+                "Баллы: 0 - краткосрочные, 1 - среднесрочные, 2 - долгосрочные."));
+            criterions.Add(
+                new Criterion("Значимость компонента для бренда",
+                "Баллы: 0 - слабая, 1 - средняя, 2 - сильная."));
+            criterions.Add(
+               new Criterion("Количество аутсорсеров на рынке",
+               "От 1-го до 10-ти (если больше, то также выставляется значение 10)"));
+            criterions.Add(
+               new Criterion("Территориальное расположение аутсорсеров",
+               "Баллы: 0 - зарубежные (большая часть), 1 - удалённый регион страны (большая часть), 2 - местные (большая часть)."));
+
+        }
+        private static void generateCriterionsThrid()
+        {
+            criterions = new List<Criterion>();
+            criterions.Add(
+                new Criterion("Опыт производства компонента",
+                "Баллы: 0 - нет, 1 - есть, по обходным технологиям, 2 - есть, предудыщего поколения/серии, 3 - есть."));
+            criterions.Add(
+                new Criterion("Является ли компонент элементом, которой потребитель отключает/снимает/заменяет/ тюнингует",
+                "Баллы: 0 - да, 1 - нет."));
+            criterions.Add(
+               new Criterion("Является ли компонент конструктивно удачным",
+               "Характеристики ощутимо превышают заданных в ТЗ(на компонент или продукт целиком, хотя бы по некоторым показателям).Баллы: 0 - нет, 1 - да."));
+            criterions.Add(
+               new Criterion("Уровень рекламаций",
+               "Баллы: 0-высокий, 1-средний, 2-слабый. При отсуствии опыта производства выставляется значение 0."));
+
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+        }
+    }
+}
---
+++ MakeOrBuy/MakeOrBuy/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace MakeOrBuy
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.labelDescriprion1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.labelDescriprion2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelDescriprion3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(6, 19);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(174, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelDescriprion1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(403, 132);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // labelDescriprion1
+            // 
+            this.labelDescriprion1.AutoSize = true;
+            this.labelDescriprion1.Location = new System.Drawing.Point(205, 19);
+            this.labelDescriprion1.Name = "labelDescriprion1";
+            this.labelDescriprion1.Size = new System.Drawing.Size(0, 13);
+            this.labelDescriprion1.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.labelDescriprion2);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Location = new System.Drawing.Point(12, 150);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(403, 132);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            // 
+            // labelDescriprion2
+            // 
+            this.labelDescriprion2.AutoSize = true;
+            this.labelDescriprion2.Location = new System.Drawing.Point(205, 19);
+            this.labelDescriprion2.Name = "labelDescriprion2";
+            this.labelDescriprion2.Size = new System.Drawing.Size(0, 13);
+            this.labelDescriprion2.TabIndex = 1;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(6, 19);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(174, 95);
+            this.listBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelDescriprion3);
+            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Location = new System.Drawing.Point(12, 288);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(403, 132);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            // 
+            // labelDescriprion3
+            // 
+            this.labelDescriprion3.AutoSize = true;
+            this.labelDescriprion3.Location = new System.Drawing.Point(205, 19);
+            this.labelDescriprion3.Name = "labelDescriprion3";
+            this.labelDescriprion3.Size = new System.Drawing.Size(0, 13);
+            this.labelDescriprion3.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(6, 19);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(174, 95);
+            this.listBox3.TabIndex = 0;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 543);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "FormMain";
+            this.Text = "Make or buy";
+
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelDescriprion1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelDescriprion2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelDescriprion3;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
